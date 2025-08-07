411b938699d78f6bb342c1df942d5d218f4a5216
isaquebrother90 <improgramcode@gmail.com>
2025-08-07 01:11:08 -0300

    feat: Initial project structure

---
+++ src/test/java/com/wefin/srm/WefinSrmMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wefin.srm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefinSrmMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
